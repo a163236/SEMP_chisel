f4856f4f3d87d5fa9e86144c3a5772a7be94f749
marumo <marumo.wataru@gmail.com>
2021-03-09 22:51:10 -0800

    marumo

--- src/main/scala/SEMP/IF_Stage/IF_Stage.scala
+++ src/main/scala/SEMP/IF_Stage/IF_Stage.scala
@@ -10,6 +10,7 @@ class IF_Stage_IO(implicit val conf: SEMPconfig) extends Bundle {
   val stall = Input(Bool())
   val flush = Input(Bool())
   val flush_tid = Input(UInt(conf.thread_width.W))  // どの実スレッドをフラッシュするか
+  val marumo = Input(Bool())
   val exception = Input(Bool())
   val pipeline = new IF_Pipeline_IO()
 }
